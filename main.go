c754b4581fafed596219c7a8d00881ebd0db8426
Thomas Fussell <thomas@fussell.io>
2021-08-12 09:41:07 -0400

    add project.go and circleci

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/giantswarm/actions-test/pkg/project"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Printf("Hello, world! %s", project.Version())
 }
-
---
+++ pkg/project/project.go
@@ -0,0 +1,29 @@
+package project
+
+var (
+	description = "Minimal binary for testing GitHub Actions."
+	gitSHA      = "n/a"
+	name        = "actions-test"
+	source      = "https://github.com/giantswarm/actions-test"
+	version     = "0.1.0-dev"
+)
+
+func Description() string {
+	return description
+}
+
+func GitSHA() string {
+	return gitSHA
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
